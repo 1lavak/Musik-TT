b1f3db510e7e561fc40193187f0571a9331b7c10
1lavak <84733246+1lavak@users.noreply.github.com>
2024-12-19 14:58:44 +0100

    Update config.js

--- config.js
+++ config.js
@@ -38,10 +38,10 @@ module.exports = {
 
   nodes: [
     {
-  "identifier": "Koi Node V4",
-  "password": "prplmoe.me",
-  "host": "lavav4.prplmoe.me",
-  "port": 1118,
+  "identifier": "INZEWORLD.COM (DE)",
+  "password": "saher.inzeworld.com",
+  "host": "lava.inzeworld.com",
+  "port": 3128,
   "secure": false
 },
  ]
