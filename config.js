34503301beef3927b1f18bdda59e467bfb03973b
1lavak <84733246+1lavak@users.noreply.github.com>
2025-02-21 11:41:28 +0100

    Update config.js

--- config.js
+++ config.js
@@ -37,8 +37,12 @@ module.exports = {
    // Lavalink Server Details
 
   nodes: [
-    { "identifier": "Public Lavalink v4 NonSSL", "password": "https://dsc.gg/ajidevserver", "host": "lava-v4.ajieblogs.eu.org", "port": 80, "secure": false },
- ]
+    {
+  "identifier": "ChalresNaig Node",
+  "password": "NAIGLAVA-dash.techbyte.host",
+  "host": "lavahatry4.techbyte.host",
+  "port": 3000,
+  "secure": false
 }
 /*
 
